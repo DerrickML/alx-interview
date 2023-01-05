9254e633d201830324fef5d26e28056a3af19acf
DeroMal <derrickmla123@gmail.com>
2023-01-05 21:46:12 +0300

    de

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,36 +1,28 @@
 #!/usr/bin/python3
-"""
-Module 0-lockboxes
-"""
+"""Exercise to practice interview"""
+from itertools import dropwhile
 
 
 def canUnlockAll(boxes):
-    """
-    You have n number of locked boxes in front of you.
-    Each box is numbered sequentially from 0 to n - 1,
-    and each box may contain keys to the other boxes.
-    Determine if all the boxes can be opened.
-    """
-    if boxes is None:
-        return False
-    if len(boxes) == 1:
-        return True
-    # Track the visited boxes
-    visited = set()
-    # The first box is unlocked
-    visited.add(0)
+    """Checks if all boxes can be unlocked"""
 
-    # Use a stack to do a Depth First Search
-    stack = []
-    stack.append(0)
+    keys = {0}
+    range_boxes = range(len(boxes))
 
-    while stack:
-        keys = boxes[stack.pop()]
-        for key in keys:
-            if key not in visited:
-                visited.add(key)
-                stack.append(key)
-    # All have been visited if the
-    # number of boxes is equal to the
-    # number of visited boxes
-    return len(boxes) == len(visited)
+    while True:
+        not_found = set()
+
+        for i in range_boxes:
+            if i in keys:
+                for key in dropwhile(lambda k: k in keys, boxes[i]):
+                    keys.add(key)
+            else:
+                not_found.add(i)
+
+        if range_boxes == not_found:
+            return False
+
+        if not not_found:
+            return True
+
+        range_boxes = not_found
