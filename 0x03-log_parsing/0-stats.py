a8e294110438a731ffa24a1559b3e2700c3ec8d2
DeroMal <derrickmla123@gmail.com>
2023-01-17 23:03:59 +0300

    de

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,59 +6,27 @@ script that reads stdin line by line and computes metrics
 import sys
 import re
 
-total_file_size = 0
-
 # sample_line = '36.196.190.72 - [2022-04-26 07:59:21.687812] '\
 #        '"GET /projects/260 HTTP/1.1" 500 878'
 
-pattern = r'^([\d]{1,3}\.){3}([\d]{1,3})'
-pattern += r'( - )(\[[\d]{4}-[\d]{2}-[\d]{2}'
-pattern += r' [\d]{2}:[\d]{2}:[\d]{2}\.[\d]{1,}\])'
-pattern += r'( "GET \/projects\/260 HTTP\/1\.1") '
-pattern += r'([\d]{3}) ([\d]{1,4})$'
+total_size = 0
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+line_count = 0
 
-
-codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-
-no_of_lines = 0
-
-
-def is_line_valid(line: str) -> bool:
-    result = re.match(pattern, line)
-    if result:
-        return True
-    return False
-
-
-def generate_statistics(line: str) -> None:
-    global total_file_size
-    chars = line.split(' ')
-
-    file_size = int(chars[-1].replace('\n', ''))
+while True:
     try:
-        status_code = int(chars[-2])
-        if status_code in codes:
-            codes[status_code] += 1
-    except ValueError:
-        pass
-
-    total_file_size += file_size
-
-
-def print_statistics() -> None:
-    print('File size: {}'.format(total_file_size))
-    for key, value in sorted(codes.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
-
-if __name__ == '__main__':
-    try:
-        for i, line in enumerate(sys.stdin, 1):
-            # generate statistics only for a valid log
-            if is_line_valid(line):
-                generate_statistics(line)
-            if not i % 10:
-                print_statistics()
+        line = input()
+        match = re.search(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[.*\] "GET /projects/260 HTTP/1.1" (\d+) (\d+)', line)
+        if match:
+            total_size += int(match.group(3))
+            status_code = int(match.group(2))
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+            line_count += 1
+        if line_count % 10 == 0:
+            print("File size: {}".format(total_size))
+            for status_code in sorted(status_codes):
+                if status_codes[status_code] != 0:
+                    print("{}: {}".format(status_code, status_codes[status_code]))
     except KeyboardInterrupt:
-        print_statistics()
-        raise
+        print("File size: {}".format(total_size))
