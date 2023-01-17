e63345508808de1ef313ce2db3354e1d726f9495
DeroMal <derrickmla123@gmail.com>
2023-01-17 23:06:54 +0300

    parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,32 +1,47 @@
 #!/usr/bin/python3
 """
-Module 0-stats
-script that reads stdin line by line and computes metrics
+Module that parses a log and prints stats to stdout
 """
-import sys
-import re
+from sys import stdin
 
-# sample_line = '36.196.190.72 - [2022-04-26 07:59:21.687812] '\
-#        '"GET /projects/260 HTTP/1.1" 500 878'
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
 
-total_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
+size = 0
 
-while True:
+
+def print_stats():
+    """Prints the accumulated logs"""
+    print("File size: {}".format(size))
+    for status in sorted(status_codes.keys()):
+        if status_codes[status]:
+            print("{}: {}".format(status, status_codes[status]))
+
+
+if __name__ == "__main__":
+    count = 0
     try:
-        line = input()
-        match = re.search(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[.*\] "GET /projects/260 HTTP/1.1" (\d+) (\d+)', line)
-        if match:
-            total_size += int(match.group(3))
-            status_code = int(match.group(2))
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-            line_count += 1
-        if line_count % 10 == 0:
-            print("File size: {}".format(total_size))
-            for status_code in sorted(status_codes):
-                if status_codes[status_code] != 0:
-                    print("{}: {}".format(status_code, status_codes[status_code]))
+        for line in stdin:
+            try:
+                items = line.split()
+                size += int(items[-1])
+                if items[-2] in status_codes:
+                    status_codes[items[-2]] += 1
+            except:
+                pass
+            if count == 9:
+                print_stats()
+                count = -1
+            count += 1
     except KeyboardInterrupt:
-        print("File size: {}".format(total_size))
+        print_stats()
+        raise
+    print_stats()
